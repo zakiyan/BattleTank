54d5b25dbb70b2f0b698c57b1559cc04bc73824f
zakiyan <j.2015.yamazaki@gmail.com>
2015-06-10 20:04:55 +0900

    ２号戦車を動かせるようにした

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+
+	private Vector3 velocity;
+	// 前進速度
+	public float forwardSpeed = 7.0f;
+	// 後退速度
+	public float backwardSpeed = 2.0f;
+	// 旋回速度
+	public float rotateSpeed = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");		
+		float v = Input.GetAxis ("Vertical");	
+
+		velocity = new Vector3 (0, 0, v);		// 上下のキー入力からZ軸方向の移動量を取得
+		// キャラクターのローカル空間での方向に変換
+		velocity = transform.TransformDirection (velocity);
+		//以下のvの閾値は、Mecanim側のトランジションと一緒に調整する
+		if (v > 0.1) {
+			velocity *= forwardSpeed;		// 移動速度を掛ける
+		} else if (v < -0.1) {
+			velocity *= backwardSpeed;	// 移動速度を掛ける
+		}
+		// 上下のキー入力でキャラクターを移動させる
+		transform.localPosition += velocity * Time.fixedDeltaTime;
+
+		// 左右のキー入力でキャラクタをY軸で旋回させる
+		transform.Rotate (0, h * rotateSpeed, 0);	
+
+//		transform.FindChild("turret").Rotate (0, h * rotateSpeed, 0);	
+
+	}
+}
