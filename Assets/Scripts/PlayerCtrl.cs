a745ab5a251b768baf82581265b5f68572302819
zakiyan <j.2015.yamazaki@gmail.com>
2015-06-10 20:10:07 +0900

    ２号戦車を動かせるようにした忘れ物

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+	const float RayCastMaxDistance = 100.0f;
+	InputManager inputManager;
+	// Use this for initialization
+	void Start () {
+		inputManager = FindObjectOfType<InputManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Walking();
+	}
+	
+	
+	
+	void Walking()
+	{
+//		if (inputManager.Clicked()) {
+			Vector2 clickPos = inputManager.GetCursorPosition();
+			// RayCastで対象物を調べる.
+			Ray ray = Camera.main.ScreenPointToRay(clickPos);
+			RaycastHit hitInfo;
+//			if(Physics.Raycast(ray,out hitInfo,RayCastMaxDistance,1 << LayerMask.NameToLayer("Ground"))) {
+			if(Physics.Raycast(ray,out hitInfo,RayCastMaxDistance)) {
+				SendMessage("SetDestination",hitInfo.point);
+//			}
+		}
+	}
+}
